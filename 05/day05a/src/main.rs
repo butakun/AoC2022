41b069b12bfa2874808574bdb4bc22b9fc52f2a4
Hiromasa Kato <hiromasa@gmail.com>
2022-12-05 07:43:50 +0100

    day 5 incomplete

---
+++ 05/day05a/src/main.rs
@@ -0,0 +1,63 @@
+use std::io::prelude::*;
+use std::io::{BufReader, SeekFrom};
+use std::fs::File;
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    let filename = &args[1];
+    let (mut stacks, moves) = read(filename);
+
+    for (n, from, to) in moves {
+        let stack_from = stacks.iter_mut().nth((from-1) as usize).unwrap();
+        let stack_to = stacks.iter_mut().nth((to-1) as usize).unwrap();
+    }
+}
+
+fn read(filename: &str) -> (Vec<Vec<char>>, Vec<(u32, u32, u32)>) {
+    let f = File::open(filename).unwrap();
+    let mut reader = BufReader::new(f);
+
+    let stack_lines: Vec<String> = reader.lines()
+        .take_while(|line| { line.as_ref().unwrap().trim().chars().next().unwrap() == '[' })
+        .map(|line| { String::from(line.unwrap()) })
+        .collect();
+    let max_height = stack_lines.len();
+    println!("{:?} max = {}", stack_lines, max_height);
+
+    let f = File::open(filename).unwrap();
+    reader = BufReader::new(f);
+    let line = reader.lines().skip(stack_lines.len()).next().unwrap().unwrap();
+    let tokens: Vec<&str> = line.trim().split(" ").filter(|b| b.len() > 0).collect();
+    let num_stacks = tokens.len();
+    println!("{num_stacks} stacks found");
+
+    let mut stacks: Vec<Vec<char>> = Vec::new();
+    for i in 0..num_stacks {
+        let mut stack: Vec<char> = Vec::new();
+        for j in 0..max_height {
+            let l = String::from(&stack_lines[max_height - j - 1]);
+            let c = l.chars().nth(1 + 4 * i).unwrap();
+            if c.is_alphabetic() {
+                stack.push(c);
+            }
+        }
+        println!("stack {i} -> {:?}", stack);
+        stacks.push(stack);
+    }
+
+    let f = File::open(filename).unwrap();
+    reader = BufReader::new(f);
+    let moves: Vec<(u32, u32, u32)> = reader.lines()
+        .skip(max_height + 1 + 1)
+        .map(|line| {
+            let line = line.unwrap();
+            let tokens: Vec<String> = line.trim().split(" ").map(|s| String::from(s)).collect();
+            let n = tokens.iter().nth(1).unwrap().parse::<u32>().unwrap();
+            let from = tokens.iter().nth(3).unwrap().parse::<u32>().unwrap();
+            let to = tokens.iter().nth(5).unwrap().parse::<u32>().unwrap();
+            (n, from, to)
+        })
+        .collect();
+
+    (stacks, moves)
+}
