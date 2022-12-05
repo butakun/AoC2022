af69c5cff31d22280633982b637084ae717b466d
Hiromasa Kato <hka@trackman.com>
2022-12-05 11:32:48 +0100

    tidied up

--- 05/day05a/src/main.rs
+++ 05/day05a/src/main.rs
@@ -9,12 +9,13 @@ fn main() {
 
     for (n, from, to) in moves {
         for i in 0..n {
-            let stack_from = stacks.iter_mut().nth((from-1) as usize).unwrap();
+            let stack_from = stacks.iter_mut().nth(from-1).unwrap();
             let c = stack_from.pop().unwrap();
-            let stack_to = stacks.iter_mut().nth((to-1) as usize).unwrap();
+            let stack_to = stacks.iter_mut().nth(to-1).unwrap();
             stack_to.push(c);
         }
-        println!("{:?}", stacks);
+        let buf: Vec<String> = stacks.iter().map(|stack| stack.into_iter().collect()).collect();
+        println!("moved {n} from {from} to {to}: {:?}", buf);
     }
 
     let mut code = String::new();
@@ -24,48 +25,56 @@ fn main() {
     println!("{code}");
 }
 
-fn read(filename: &str) -> (Vec<Vec<char>>, Vec<(u32, u32, u32)>) {
+fn read(filename: &str) -> (Vec<Vec<char>>, Vec<(usize, usize, usize)>) {
     let f = File::open(filename).unwrap();
     let mut reader = BufReader::new(f);
 
-    let stack_lines: Vec<String> = reader.lines()
-        .take_while(|line| { line.as_ref().unwrap().trim().chars().next().unwrap() == '[' })
-        .map(|line| { String::from(line.unwrap()) })
+    let stack_lines: Vec<String>;
+    let mut stack_ids: Vec<usize> = Vec::new();
+    stack_lines = reader.by_ref().lines()
+        .take_while(|line| {
+            let line = line.as_ref().unwrap();
+            let c = line.trim().chars().next().unwrap();
+            if c == '[' {
+                true
+            } else {
+                stack_ids = line.trim()
+                    .split(" ")
+                    .filter(|b| b.len() > 0)
+                    .map(|b| b.parse().unwrap())
+                    .collect();
+                false
+            }
+        })
+        .map(|line| String::from(line.unwrap()))
         .collect();
-    let max_height = stack_lines.len();
-    println!("{:?} max = {}", stack_lines, max_height);
 
-    let f = File::open(filename).unwrap();
-    reader = BufReader::new(f);
-    let line = reader.lines().skip(stack_lines.len()).next().unwrap().unwrap();
-    let tokens: Vec<&str> = line.trim().split(" ").filter(|b| b.len() > 0).collect();
-    let num_stacks = tokens.len();
-    println!("{num_stacks} stacks found");
+    let max_height = stack_lines.len();
 
     let mut stacks: Vec<Vec<char>> = Vec::new();
-    for i in 0..num_stacks {
-        let mut stack: Vec<char> = Vec::new();
+    for stack_id in stack_ids {
+        stacks.push(Vec::new());
+    }
+
+    for (i, stack) in stacks.iter_mut().enumerate() {
         for j in 0..max_height {
-            let l = String::from(&stack_lines[max_height - j - 1]);
+            let l = &stack_lines[max_height - j - 1];
             let c = l.chars().nth(1 + 4 * i).unwrap();
             if c.is_alphabetic() {
                 stack.push(c);
             }
         }
-        println!("stack {i} -> {:?}", stack);
-        stacks.push(stack);
     }
 
-    let f = File::open(filename).unwrap();
-    reader = BufReader::new(f);
-    let moves: Vec<(u32, u32, u32)> = reader.lines()
-        .skip(max_height + 1 + 1)
+    let moves: Vec<(usize, usize, usize)> = reader.by_ref().lines().skip(1)
         .map(|line| {
             let line = line.unwrap();
-            let tokens: Vec<String> = line.trim().split(" ").map(|s| String::from(s)).collect();
-            let n = tokens.iter().nth(1).unwrap().parse::<u32>().unwrap();
-            let from = tokens.iter().nth(3).unwrap().parse::<u32>().unwrap();
-            let to = tokens.iter().nth(5).unwrap().parse::<u32>().unwrap();
+            let tokens: Vec<String> = line.trim().split(" ")
+                .map(|s| String::from(s))
+                .collect();
+            let n: usize = tokens.iter().nth(1).unwrap().parse().unwrap();
+            let from: usize = tokens.iter().nth(3).unwrap().parse().unwrap();
+            let to: usize = tokens.iter().nth(5).unwrap().parse().unwrap();
             (n, from, to)
         })
         .collect();
