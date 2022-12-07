271b6d34ceaf6d87e2284b44fc969d569887901a
Hiromasa Kato <hka@trackman.com>
2022-12-07 09:40:08 +0100

    cleaned up

--- 07/python/07_1.py
+++ 07/python/07_1.py
@@ -1,69 +1,60 @@
 from pprint import pprint
 
 
-def read(filename):
-
-    def read_dir(lines, path, tree, node):
-        print(f"PATH {path}")
-        pprint(f"TREE {tree}")
-
-        for j, line in enumerate(lines):
-            tokens = line.strip().split(" ")
-            if tokens[0] == "$":
-                if tokens[1] == "cd":
-                    if tokens[2] == "..":
-                        print("popping:", path)
-                        path.pop()
-                        node = node[".."]
-                    else:
-                        path.append(tokens[2])
-                        node = node[tokens[2]]
-                elif tokens[1] == "ls":
-                    pass
+def read_dir(lines, node):
+    for j, line in enumerate(lines):
+        tokens = line.strip().split(" ")
+        if tokens[0] == "$":
+            if tokens[1] == "cd":
+                dirname = tokens[2]
+                if dirname == "..":
+                    node = node[".."]
                 else:
-                    raise ValueError(line)
+                    node = node[dirname]
+            elif tokens[1] == "ls":
+                pass
             else:
-                if tokens[0] == "dir":
-                    node[tokens[1]] = {"..": node}
-                else:
-                    node[tokens[1]] = int(tokens[0])
-                print("adding ", path[-1], " : ", tokens[1])
+                raise ValueError(line)
+        else:
+            if tokens[0] == "dir":
+                node[tokens[1]] = {"..": node}
+            else:
+                node[tokens[1]] = int(tokens[0])
+
+
+def calc_size(node, book, path):
+    siz = 0
+    for k, v in node.items():
+        if k == "..":
+            continue
+        if isinstance(v, dict):
+            path.append(k)
+            siz_child = calc_size(v, book, path)
+            siz += siz_child
+            if siz_child <= 100000:
+                key = "/".join(path)
+                assert key not in book
+                book[key] = siz_child
+                path.pop()
+                continue
+            continue
+        siz += v
+    return siz
+
+
+def main(filename):
 
     lines = [l for l in open(filename)]
-    path = []
     tree = {"/": {}, "..": None}
-    read_dir(lines, path, tree, tree)
+    read_dir(lines, tree)
     pprint(tree)
 
-    def calc_size(node, book):
-        siz = 0
-        for k, v in node.items():
-            if k == "..":
-                continue
-            if isinstance(v, dict):
-                siz_child = calc_size(v, book)
-                siz += siz_child
-                if siz_child <= 100000:
-                    count = 0
-                    while True:
-                        key = f"{k}.{count}"
-                        if key not in book:
-                            break
-                        print(key, "exists")
-                        count += 1
-                    book[key] = siz_child
-                continue
-            siz += v
-        return siz
-
     book = {}
-    siz = calc_size(tree, book)
+    path = []
+    siz = calc_size(tree, book, path)
     pprint(book)
 
     print("total = ", sum(book.values()))
-
-def main(filename):
-    read(filename)
 
 
 if __name__ == "__main__":
--- 07/python/07_2.py
+++ 07/python/07_2.py
@@ -1,54 +1,51 @@
 from pprint import pprint
 
 
-def main(filename):
-
-    def read_dir(lines, path, node):
-        print(f"PATH {path}")
-        pprint(f"TREE {tree}")
-
-        for j, line in enumerate(lines):
-            tokens = line.strip().split(" ")
-            if tokens[0] == "$":
-                if tokens[1] == "cd":
-                    if tokens[2] == "..":
-                        path.pop()
-                        node = node[".."]
-                    else:
-                        path.append(tokens[2])
-                        node = node[tokens[2]]
-                elif tokens[1] == "ls":
-                    pass
+def read_dir(lines, node):
+    for j, line in enumerate(lines):
+        tokens = line.strip().split(" ")
+        if tokens[0] == "$":
+            if tokens[1] == "cd":
+                dirname = tokens[2]
+                if dirname == "..":
+                    node = node[".."]
                 else:
-                    raise ValueError(line)
+                    node = node[dirname]
+            elif tokens[1] == "ls":
+                pass
             else:
-                if tokens[0] == "dir":
-                    node[tokens[1]] = {"..": node}
-                else:
-                    node[tokens[1]] = int(tokens[0])
+                raise ValueError(line)
+        else:
+            if tokens[0] == "dir":
+                node[tokens[1]] = {"..": node}
+            else:
+                node[tokens[1]] = int(tokens[0])
 
+
+def calc_size(node, book, path):
+    siz = 0
+    for k, v in node.items():
+        if k == "..":
+            continue
+        if isinstance(v, dict):
+            path.append(k)
+            siz_child = calc_size(v, book, path)
+            siz += siz_child
+            key = "/".join(path)
+            assert key not in book
+            book[key] = siz_child
+            path.pop()
+            continue
+        siz += v
+    return siz
+
+
+def main(filename):
     lines = [l for l in open(filename)]
     path = []
     tree = {"/": {}, "..": None}
-    read_dir(lines, path, tree)
+    read_dir(lines, tree)
     pprint(tree)
-
-    def calc_size(node, book, path):
-        siz = 0
-        for k, v in node.items():
-            if k == "..":
-                continue
-            if isinstance(v, dict):
-                path.append(k)
-                siz_child = calc_size(v, book, path)
-                siz += siz_child
-                key = "/".join(path)
-                assert key not in book
-                book[key] = siz_child
-                path.pop()
-                continue
-            siz += v
-        return siz
 
     book = {}
     path = []
