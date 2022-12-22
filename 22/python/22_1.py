f1f73016c4d6332790412f6b239fabba86580284
Hiromasa Kato <hka@trackman.com>
2022-12-22 10:15:38 +0100

    part 1

--- 22/python/22_1.py
+++ 22/python/22_1.py
@@ -1,5 +1,5 @@
 import numpy as np
-import re
+import logging
 
 
 class Grid(object):
@@ -15,15 +15,40 @@ class Grid(object):
         # no diagonal dij
         ij1 = ij + dij
         i1, j1 = ij1
-        if ij1[0] < self.imins[j1]:
-            ij1[0] = imaxs[j1]
-        elif ij1[0] > self.imaxs[j1]:
-            ij1[0] = imins[j1]
-        elif ij1[1] < self.imins[j1]:
-            ij1[1] = imaxs[j1]
-        elif ij1[1] > self.imaxs[j1]:
-            ij1[1] = imins[j1]
-        return ij1
+        if dij[0] != 0 and dij[1] == 0:
+            if ij1[0] < self.imins[ij1[1]]:
+                ij1[0] = self.imaxs[ij1[1]]
+                logging.info(f"hit wall to left, ij1 wrap around to {ij1}")
+            elif ij1[0] > self.imaxs[ij1[1]]:
+                ij1[0] = self.imins[ij1[1]]
+                logging.info("hit wall to right, ij1 wrap around to {ij1}")
+        elif dij[0] == 0 and dij[1] != 0:
+            if ij1[1] < self.jmins[ij1[0]]:
+                ij1[1] = self.jmaxs[ij1[0]]
+                logging.info("hit wall up, ij1 wrap around to {ij1}")
+            elif ij1[1] > self.jmaxs[ij1[0]]:
+                ij1[1] = self.jmins[ij1[0]]
+                logging.info("hit wall down, ij1 wrap around to {ij1}")
+        else:
+            raise ValueError(f"dij diagonal {dij}")
+
+        if self.grid[ij1[0], ij1[1]] == 0:
+            ij = ij1
+        else:
+            logging.info("but can't move there so staying put at {ij}")
+        return ij
+
+    def prettified(self, path=None):
+        direc_to_char = { "R": ">", "U": "^", "L": "<", "D": "v" }
+        chars = { 0: ".", 1: "#", 2: " " }
+        buf = [ [ chars[c] for c in line ] for line in self.grid.T ]
+        if path:
+            for (i, j), direc in path:
+                buf[j][i] = direc_to_char[direc]
+
+        buf = "\n".join([ "".join([c for c in line]) for line in buf ])
+
+        return buf
 
 
 def read(filename):
@@ -44,6 +69,12 @@ def read(filename):
             ops.append(int(token))
             ops.append(c)
             token = ""
+    if token:
+        if token.isdigit():
+            ops.append(int(token))
+        else:
+            assert token == "L" or token == "R"
+            ops.append(token)
 
     grid = np.zeros((idim, jdim), np.uint8)
     for j, line in enumerate(lines[:-2]):
@@ -51,8 +82,8 @@ def read(filename):
             grid[i, j] = 0 if c == "." else 1 if c == "#" else 2
         grid[len(line):, j] = 2
 
-    istart = 0
-    jstart = np.where(grid[istart, :] == 0)[0][0]
+    jstart = 0
+    istart = np.where(grid[:, jstart] == 0)[0][0]
 
     direc = "R"
 
@@ -99,7 +130,7 @@ def one_op(G, ij, direc, op):
             direc = "R"
     elif op == "R":
         if direc == "R":
-            direct = "D"
+            direc = "D"
         elif direc == "U":
             direc = "R"
         elif direc == "L":
@@ -108,37 +139,41 @@ def one_op(G, ij, direc, op):
             direc = "L"
     else:
         if direc == "R":
-            dij = np.array([1, 0], np.uint8)
+            dij = np.array([1, 0], np.int32)
         elif direc == "U":
-            dij = np.array([0, -1], np.uint8)
+            dij = np.array([0, -1], np.int32)
         elif direc == "L":
-            dij = np.array([-1, 0], np.uint8)
+            dij = np.array([-1, 0], np.int32)
         elif direc == "D":
-            dij = np.array([0, 1], np.uint8)
+            dij = np.array([0, 1], np.int32)
         else:
             raise ValueError(direc)
 
         ijnext = ij.copy()
         for step in range(op):
-            ij_ = G.one_step(ijnext, dij)
-            c_ = G.grid[ij_[0], ij_[1]] 
-            print("ij_, c = ", ij_, c_)
-            if c_ == 0:
-                ijnext = ij_
-            elif c_ == 1:
-                break
+            ijnext = G.one_step(ijnext, dij)
 
         ij = ijnext
-    return ij
+    return ij, direc
 
 
 def main(filename):
     G, ops, ij0, direc = read(filename)
 
+    print(G.prettified())
+
+    path = [(ij0, direc)]
     ij = ij0
     for op in ops:
-        ij = one_op(G, ij, direc, op)
-        print(op, ij)
+        ij, direc = one_op(G, ij, direc, op)
+        path.append((ij, direc))
+        print(op, ij, direc)
+
+    print(G.prettified(path))
+
+    direc_to_digit = { "R": 0, "D": 1, "L": 2, "U": 3 }
+    password = 1000 * (ij[1] + 1) + 4 * (ij[0] + 1) + direc_to_digit[direc]
+    print(password)
 
 
 if __name__ == "__main__":
